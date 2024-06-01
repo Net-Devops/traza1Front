e5162baec0fc8ba62a15ab7a1ba7c20705b70a53
maxigomez99 <gomez.maximiliano99@gmail.com>
2024-05-31 21:01:39 -0300

    scroll agregado

--- src/components/element/menuLateral/Sider.tsx
+++ src/components/element/menuLateral/Sider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {  SettingOutlined, LogoutOutlined, BankOutlined, FileOutlined, HomeOutlined, DollarCircleOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { SettingOutlined, LogoutOutlined, BankOutlined, FileOutlined, HomeOutlined, DollarCircleOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import {  Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
 import { Dropdown, Button } from 'antd';
 import { UserOutlined, } from '@ant-design/icons';
 import { Link } from "react-router-dom";
@@ -28,19 +28,19 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem('/',"HOME", "1", <HomeOutlined /> ),
-  getItem("/empresas","EMPRESA", "2", <BankOutlined />),
-  getItem("/productos","PRODUCTOS", "sub1", <ShoppingCartOutlined />, [
-    getItem("/productos","LISTA DE PRODUCTOS", "3"),
-    getItem("/categorias","CATEGORIAS", "4"),
+  getItem('/', "HOME", "1", <HomeOutlined />),
+  getItem("/empresas", "EMPRESA", "2", <BankOutlined />),
+  getItem("/productos", "PRODUCTOS", "sub1", <ShoppingCartOutlined />, [
+    getItem("/productos", "LISTA DE PRODUCTOS", "3"),
+    getItem("/categorias", "CATEGORIAS", "4"),
   ]),
-  getItem("/promociones","PROMOCIONES", "5", <DollarCircleOutlined />),
-  getItem("/empleados","EMPLEADOS", "sub2", <ShoppingCartOutlined />, [
-    getItem("/empleados","LISTA DE EMPLEADOS", "6"),
-    getItem("/roles","ROLES", "7"),
+  getItem("/promociones", "PROMOCIONES", "5", <DollarCircleOutlined />),
+  getItem("/empleados", "EMPLEADOS", "sub2", <ShoppingCartOutlined />, [
+    getItem("/empleados", "LISTA DE EMPLEADOS", "6"),
+    getItem("/roles", "ROLES", "7"),
   ]),
 
-  getItem("/insumos","INSUMOS", "8", <FileOutlined />),
+  getItem("/insumos", "INSUMOS", "8", <FileOutlined />),
 ];
 
 const App: React.FC = () => {
@@ -50,21 +50,21 @@ const App: React.FC = () => {
 
   const menu = (
     <Menu onClick={handleMenuClick}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-            Perfil
-        </Menu.Item>
-        <Menu.Item key="2" icon={<SettingOutlined />}>
-            Ajustes
-        </Menu.Item>
-        <Menu.Item key="3" icon={<LogoutOutlined />}>
-            Cerrar Sesión
-        </Menu.Item>
+      <Menu.Item key="1" icon={<UserOutlined />}>
+        Perfil
+      </Menu.Item>
+      <Menu.Item key="2" icon={<SettingOutlined />}>
+        Ajustes
+      </Menu.Item>
+      <Menu.Item key="3" icon={<LogoutOutlined />}>
+        Cerrar Sesión
+      </Menu.Item>
     </Menu>
   );
 
   return (
     <Layout>
-      <Header style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', height:"50px" }}>
+      <Header style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', height: "50px" }}>
         <div className="demo-logo" />
         <Dropdown overlay={menu}>
           <Button>
@@ -73,7 +73,7 @@ const App: React.FC = () => {
         </Dropdown>
       </Header>
       <Layout>
-        <Sider width={'15%'} style={{ background: colorBgContainer, height: '90vh', marginTop:'0.7%',  }}>
+        <Sider width={'15%'} style={{ background: colorBgContainer, height: '90vh', marginTop: '0.7%', }}>
           <Menu
             mode="inline"
             defaultSelectedKeys={['1']}
@@ -82,7 +82,7 @@ const App: React.FC = () => {
             items={items}
           />
         </Sider>
-        <Layout style={{ padding: '0 24px 24px' , marginTop:'0.7%', height:'93vh'}}>
+        <Layout style={{ padding: '0 24px 24px', marginTop: '0.7%', height: '93vh' }}>
           <Content
             style={{
               padding: 24,
@@ -90,6 +90,7 @@ const App: React.FC = () => {
               minHeight: 280,
               background: colorBgContainer,
               borderRadius: borderRadiusLG,
+              overflow: 'auto', // Add this line
             }}
           >
             <Rutas />
