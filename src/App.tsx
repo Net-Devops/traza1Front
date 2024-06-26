b398a6702127ff09bac3ede5983c408171051cf8
Sallei <nicosallei@gmail.com>
2024-06-25 23:21:19 -0300

    actualizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store";
-import Sider from "./components/element/menuLateral/Sider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Sider from "./components/element/menuLateral/Sider";
 import Rutas from "./routes/Routes"; // Asegúrate de importar correctamente el componente de rutas
 
 const AppContent = () => {
@@ -20,14 +21,15 @@ const AppContent = () => {
   );
 };
 
-function App() {
-  return (
+const App = () => (
+  <React.StrictMode>
     <Provider store={store}>
       <Router>
         <AppContent />
       </Router>
     </Provider>
-  );
-}
+  </React.StrictMode>
+);
 
-export default App;
+// Utiliza ReactDOM.render para renderizar la aplicación
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/pages/estadistica/Estadistica.tsx
+++ src/components/pages/estadistica/Estadistica.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Select, Button, Table, Form } from "antd";
 import { Empresas, getEmpresas } from "../../../service/ServiceEmpresa";
 import { getSucursal, Sucursal } from "../../../service/ServiceSucursal";
